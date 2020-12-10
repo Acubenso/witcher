4cd72ba649a229c3d691792caeca8fdf8e8c6072
Erik Kopcha <erickopcha@gmail.com>
2020-12-10 18:07:29 +0200

    Update seriesDB.js

--- js/seriesDB.js
+++ js/seriesDB.js
@@ -1,24 +1,24 @@
 let seriesData = [
   {
-    image: '../img/card-image.jpg',
+    image: './img/card-image.jpg',
     episode: 1,
     seriesName: 'Початок кінця',
     iframeSrc: 'https://www.youtube.com/embed/-sfZuonnKe8'
   },
   {
-    image: '../img/card-image2x.jpg',
+    image: './img/card-image2x.jpg',
     episode: 2,
     seriesName: 'Чотири знаки',
     iframeSrc: 'https://www.youtube.com/embed/eb90gqGYP9c'
   },
   {
-    image: '../img/card-image3x.jpg',
+    image: './img/card-image3x.jpg',
     episode: 3,
     seriesName: 'Зняти маски',
     iframeSrc: 'https://www.youtube.com/embed/cSqi-8kAMmM'
   },
   {
-    image: '../img/card-image4x.jpg',
+    image: './img/card-image4x.jpg',
     episode: 4,
     seriesName: 'Розслідування',
     iframeSrc: 'https://www.youtube.com/embed/A-mCKNTU_mU'
